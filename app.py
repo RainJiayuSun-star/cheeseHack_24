e1f7348070e95b236661bb74fb7636b9b57865a9
RainJiayuSun-star <145049305+RainJiayuSun-star@users.noreply.github.com>
2024-11-17 07:32:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -120,8 +120,11 @@ def predict():
         })
     '''
 
+#if __name__ == '__main__':
+#    app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
+
 '''from flask import Flask, request, jsonify
 import pandas as pd
 import numpy as np
